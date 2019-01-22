e5cda8c188068a32ea242f91608a47bb94e3f1a2
VINOD HP <VINOD HP@VINOD>
2019-01-22 15:24:39 +0530

    Second push

--- MavenProject/src/test/java/MagentoTest.java
+++ MavenProject/src/test/java/MagentoTest.java
@@ -3,7 +3,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest 
@@ -12,7 +12,7 @@ public class MagentoTest
  public void magento()
  {
 	 String url="http://www.magento.com";
-	 WebDriver driver=new ChromeDriver();
+	 WebDriver driver=new FirefoxDriver();
 	 driver.manage().window().maximize();
 	 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	 driver.get(url);
